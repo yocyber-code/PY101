b1d0be2012656e157fc2225943a4c0040db8059d
yocyber-code <yocyberyoshi@gmail.com>
2023-08-30 23:36:23 +0700

    'commit'

--- exercise6/EF_NT_01.py
+++ exercise6/EF_NT_01.py
@@ -1,23 +1,33 @@
 import math
+import sys
 
 total_party = int(input())
 votes = []
-while len(votes) < total_party:
-    party_vote = str(input()).split(' ')
-    for i in range(len(party_vote)):
-        party_vote[i] = int(party_vote[i])
-    votes.append(party_vote)
+strArr = []
+for i in range(total_party):
+    party_vote = input()
+    strArr.append(party_vote)
+
+for i in range(len(strArr)):
+    strSplit = strArr[i].split(" ")
+    vote_cast_int = []
+    for j in range(len(strSplit)):
+        vote_cast_int.append(int(strSplit[j]))
+    votes.append(vote_cast_int)
 
 total_vote = 0
-for party_vote in votes:
-    if party_vote[2] != 0:
-        total_vote += party_vote[0]
+for i in range(total_party):
+    if votes[i][2] > 0:
+        total_vote += votes[i][0]
 
-quota_score = total_vote / 500
+quota_score = total_vote / 500.0
 target_parliament_member = []
 
-for party_vote in votes:
-    target_parliament_member.append(party_vote[0] / quota_score)
+for i in range(total_party):
+    if votes[i][0] <= 0:
+        target_parliament_member.append(0)
+    else:
+        target_parliament_member.append(votes[i][0] / quota_score)
 
 partyList_Step_1 = []
 for i in range(total_party):
@@ -37,7 +47,7 @@ for i in range(len(partyList_Step_2)):
     min1 = min(math.floor(target_parliament_member[i]), partyList_Step_2[i])
     partyList_Step_3.append(min1)
 
-total_party_list = sum(partyList_Step_2)
+total_party_list = sum(partyList_Step_3)
 
 if total_party_list > 150:
     partyList_Step_4 = []
@@ -65,34 +75,17 @@ if total_party_list < 150:
         if len(max_index_list) == total_party:
             multiple += 1
         if len(max_index_list) > 1:
-            avg_target = 0
+            avg_target = -sys.maxsize - 1
             max_index_list_temp = max_index_list.copy()
             max_index_list.clear()
             for i in max_index_list_temp:
-                avg_temp = votes[i][0] / (votes[i][0] / quota_score)
-                if avg_temp >= avg_target:
-                    avg_target = avg_temp
+                avg_score_per_people = votes[i][0] / \
+                    (votes[i][0] / quota_score)
+                if avg_score_per_people >= avg_target:
+                    avg_target = avg_score_per_people
                     max_index_list.append(i)
-            # v_max = []
-            # for i in range(total_party):
-            #     v_max.append(votes[i][0] / (votes[i][0] / quota_score))
-
-            # max_target = 0
-            # target_max_index = [0]
-            # for i in range(1, total_party, 1):
-            #     if i in max_index_list:
-            #         if v_max[max_target] == v_max[i]:
-            #             target_max_index.append(i)
-            #         else:
-            #             max_target = i
-            #             target_max_index.clear()
-            #             target_max_index.append(i)
-
-            # if len(target_max_index) >= 1:
-            #     for i in target_max_index:
-            #         if partyList_Step_3[i] + 1 <= target_parliament_member[i]:
-            #             max_index_list[0] = i
-            #             break
+        if len(max_index_list) == 0:
+            print("Error")
         for index in max_index_list:
             if partyList_Step_3[index] + 1 <= target_parliament_member[index]:
                 partyList_Step_3[index] += 1
@@ -104,4 +97,3 @@ if total_party_list < 150:
 
 for i in range(total_party):
     print(votes[i][1] + partyList_Step_3[i])
- 
